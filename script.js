acc86e567a3b64177506f8d2f191214a8785324b
codetech-arindam <arindam.das70986@gmail.com>
2026-05-22 05:13:30 +0530

    Add smartphone and tablet options to LocomotiveScroll

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const locomotiveScroll = new LocomotiveScroll({
   lenisOptions: {
     wrapper: document.querySelector(".scroll-container"),
+    smartphone: {
+      smooth: false,
+    },
+    tablet: {
+      smooth: false,
+    },
   },
 });
