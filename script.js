60e35f5b5ffb8c9c932620eb89200de35b00cbec
codetech-arindam <arindam.das70986@gmail.com>
2026-07-15 22:39:41 +0000

    del

--- script.js
+++
@@ -1,6 +0,0 @@
-const locomotiveScroll = new LocomotiveScroll({
-  lenisOptions: {
-    wrapper: document.querySelector(".scroll-container"),
-    content: document.querySelector(".page1"),
-  },
-});
